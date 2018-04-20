aa9e721cefa3c63932641e1847cbd7ac824bf057
nick-chal <nickguy.s@gmail.com>
2018-04-20 17:24:43 +0545

    Second Day with the YelpCamp. Show route and addition of the description

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 //SCHEMA setup
 var campSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campSchema);
@@ -36,11 +37,11 @@ var campgrounds = [
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'YelpCamp' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'YelpCamp' });
+// });
 
-router.get('/campgrounds', function (req, res, next) {
+router.get('/', function (req, res, next) {
     Campground.find({}, function (err, allCampgrounds) {
         if(err){
             console.log("Error Occured in find");
@@ -51,10 +52,15 @@ router.get('/campgrounds', function (req, res, next) {
     //res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+router.get("/campgrounds",function (req, res, next) {
+    res.redirect('/');
+});
+
 router.post("/campgrounds", function (req, res, next) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
     Campground.create(newCampground, function (err, newCampAdded) {
        if(err){
            console.log("Error Occured in new addition");
@@ -68,6 +74,16 @@ router.post("/campgrounds", function (req, res, next) {
 
 router.get("/campgrounds/new", function (req, res, next) {
    res.render("new");
+});
+
+router.get("/campgrounds/:id", function (req, res, next) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+       if(err){
+           console.log("Error finding the camp by id");
+       } else {
+           res.render("show", {campground: foundCampground});
+       }
+    });
 });
 
 
